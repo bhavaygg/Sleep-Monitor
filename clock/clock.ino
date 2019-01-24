4116835599499ae81c4e76b6852f79572927ac87
Chokerino <43617111+Chokerino@users.noreply.github.com>
2019-01-24 13:42:19 +0530

    base

--- clock/clock.ino
+++ clock/clock.ino
@@ -1,6 +1,7 @@
 #include "Wire.h"
 #define DS3231_I2C_ADDRESS 0x68
-
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2);
 // Convert normal decimal numbers to binary coded decimal
 byte decToBcd(byte val){
   return( (val/10*16) + (val%10) );
@@ -15,6 +16,8 @@ void setup(){
   // set the initial time here:
   // DS3231 seconds, minutes, hours, day, date, month, year
   //setDS3231time(0,15,13,4,23,1,19);
+  lcd.init(); //initialize the lcd
+  lcd.backlight(); //open the backlight 
 }
 void setDS3231time(byte second, byte minute, byte hour, byte dayOfWeek, byte
 dayOfMonth, byte month, byte year){
@@ -55,47 +58,72 @@ void displayTime(){
   // retrieve data from DS3231
   readDS3231time(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month,
   &year);
+  lcd.setCursor(0,0);
+  lcd.print(hour,DEC);
   // send it to the serial monitor
   Serial.print(hour, DEC);
   // convert the byte variable to a decimal number when displayed
+  lcd.setCursor(1,0);
   Serial.print(":");
+  lcd.print(":");
   if (minute<10){
     Serial.print("0");
+    
+    lcd.print("0");
   }
   Serial.print(minute, DEC);
   Serial.print(":");
+  lcd.setCursor(3,0);
+  lcd.print(minute, DEC);
+  lcd.setCursor(4,0);
+  lcd.print(":");
   if (second<10){
     Serial.print("0");
+    lcd.print("0");
   }
   Serial.print(second, DEC);
   Serial.print(" ");
+  lcd.print(second,DEC);
+  lcd.print(" ");
   Serial.print(dayOfMonth, DEC);
   Serial.print("/");
+  lcd.print(dayOfMonth,DEC);
+  lcd.print("/");
   Serial.print(month, DEC);
   Serial.print("/");
+  lcd.print(month,DEC);
+  lcd.print("/");
   Serial.print(year, DEC);
+  lcd.print(year,DEC);
   Serial.print(" Day of week: ");
   switch(dayOfWeek){
   case 1:
     Serial.println("Sunday");
+    lcd.print("Sunday");
     break;
   case 2:
     Serial.println("Monday");
+    lcd.print("Monday");
     break;
   case 3:
     Serial.println("Tuesday");
+    lcd.print("Tuesday");
     break;
   case 4:
     Serial.println("Wednesday");
+    lcd.print("Wednesday");
     break;
   case 5:
     Serial.println("Thursday");
+    lcd.print("Thursday");
     break;
   case 6:
     Serial.println("Friday");
+    lcd.print("Friday");
     break;
   case 7:
     Serial.println("Saturday");
+    lcd.print("Saturday");
     break;
   }
 }
